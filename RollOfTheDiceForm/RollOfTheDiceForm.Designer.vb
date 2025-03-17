f78e86043bd9ca3cf53e8691ca88cc00d8b4c281
rc-earnest <rc.earnest04@gmail.com>
2025-03-16 21:14:52 -0600

    Added code to randomize and display the two rolled dice, and added funcitonality to the clear and exit buttons.

--- RollOfTheDiceForm/RollOfTheDiceForm.Designer.vb
+++ RollOfTheDiceForm/RollOfTheDiceForm.Designer.vb
@@ -22,10 +22,72 @@ Partial Class RollOfTheDiceForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.Font = New System.Drawing.Font("Courier New", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 20
+        Me.DisplayListBox.Location = New System.Drawing.Point(34, 37)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(739, 304)
+        Me.DisplayListBox.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Font = New System.Drawing.Font("Roboto Condensed", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RollButton.Location = New System.Drawing.Point(402, 382)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(113, 56)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "&Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Font = New System.Drawing.Font("Roboto Condensed", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClearButton.Location = New System.Drawing.Point(521, 382)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(113, 56)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Font = New System.Drawing.Font("Roboto Condensed", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(640, 382)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(113, 56)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'RollOfTheDiceForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Name = "RollOfTheDiceForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
 End Class
--- RollOfTheDiceForm/RollOfTheDiceForm.vb
+++ RollOfTheDiceForm/RollOfTheDiceForm.vb
@@ -4,5 +4,58 @@
 'Roll of the Dice Form
 'git link
 Public Class RollOfTheDiceForm
+    Private Sub RollOfTheDiceForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Roll of the Dice"
+        ToolTip1.SetToolTip(RollButton, "Rolls 2 dice 1,000 times and displays how many times each number was rolled.")
+        ToolTip1.SetToolTip(ClearButton, "Clears the Display of all numbers rolled.")
+        ToolTip1.SetToolTip(ExitButton, "Exits the Program.")
+        SetDefaults()
+    End Sub
+    Sub SetDefaults()
+        RollButton.Focus()
+        DisplayListBox.Items.Clear()
+        DisplayListBox.Items.Add("Roll of the Dice".PadLeft(10))
+    End Sub
 
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim counts(10) As Integer
+        Dim firstDie As Integer
+        Dim secondDie As Integer
+        Dim sum As Integer
+        Dim header As String
+        Dim data As String
+        For i = 0 To 999
+            firstDie = randomNumberBetween(1, 6)
+            secondDie = randomNumberBetween(1, 6)
+            sum = firstDie + secondDie
+            counts(sum - 2) += 1
+        Next
+        DisplayListBox.Items.Add(StrDup(68, "-"))
+        For i = 2 To 12
+            header += (CStr(i).PadLeft(5) & "|")
+        Next
+        DisplayListBox.Items.Add(header)
+        For i = 0 To 10
+            data += (CStr(counts(i)).PadLeft(5) & "|")
+        Next
+        DisplayListBox.Items.Add(data)
+        DisplayListBox.Items.Add(StrDup(68, "-"))
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        SetDefaults()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Function randomNumberBetween(min As Integer, max As Integer) As Integer
+        Randomize()
+        Dim randomNumber As Single
+        randomNumber = Rnd()
+        randomNumber *= max - min + 1
+        randomNumber += min - 1
+        Return CInt(Math.Ceiling(randomNumber))
+    End Function
 End Class
